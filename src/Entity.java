1be0970618f2e8b362b5aa35df3afcc2611ac821
Dhrushit Raval <dr9703@rit.edu>
2022-02-19 17:37:53 -0500

    adding default properties

--- src/Entity.java
+++ src/Entity.java
@@ -1,12 +1,25 @@
-public class Entity {
+public abstract class Entity {
 
     BoundingBox boundingBox;
+    protected Color baseColor;
+    protected Point position;
 
-    public boolean intersect(Ray ray) {
+    public Entity(Color baseColor, Point position) {
+        this.setBaseColor(baseColor);
+        this.position = position;
+    }
+
+    public abstract boolean intersect(Ray ray);
+
+    public boolean intersect(Entity entity) {
         return false;
     }
 
-    public boolean intersect(Entity entity){
-        return false;
+    protected void setBaseColor(Color baseColor) {
+        if (baseColor == null) {
+            this.baseColor = new Color(255, 255, 255);
+        } else {
+            this.baseColor = baseColor;
+        }
     }
 }
