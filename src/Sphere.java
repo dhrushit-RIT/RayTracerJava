8354e440e257c08fd9c48eab810ebce1c1f0b67d
Dhrushit Raval <dr9703@rit.edu>
2022-02-19 17:39:19 -0500

    intersection definition

--- src/Sphere.java
+++ src/Sphere.java
@@ -1,25 +1,43 @@
+
 public class Sphere extends Entity {
+
     private Point wCenter;
     private Point nCenter;
-    private float radius;
-    private Color baseColor;
 
-    @Override
+    private double radius;
+
+    public Sphere(Point wCenter, double radius, Color baseColor) {
+        super(baseColor, new Point(wCenter));
+        this.wCenter = wCenter;
+        this.radius = radius;
+    }
+
     public boolean intersect(Ray ray) {
-        Vector direction = ray.getDirection();
-        float rx = direction.xNormalized();
-        float ry = direction.yNormalized();
-        float rz = direction.zNormalized();
+        Vector rDirection = ray.getDirection();
+        Point rOrigin = ray.getOrigin();
 
-        double xDiff = ray.origin.x - center.x;
-        double yDiff = ray.origin.y - center.y;
-        double zDiff = ray.origin.z - center.z;
+        Point centerCamSpace = Camera.toCameraSpace(wCenter);
+        Point rOriginCamSpace = Camera.toCameraSpace(rOrigin);
+        Vector rDirectionCamSpace = Camera.toCameraSpace(rDirection);
+
+        // System.out.println(rOriginCamSpace + " " + centerCamSpace);
+
+        double rx = rDirectionCamSpace.xNormalized();
+        double ry = rDirectionCamSpace.yNormalized();
+        double rz = rDirectionCamSpace.zNormalized();
+
+        double xDiff = rOriginCamSpace.x - centerCamSpace.x;
+        double yDiff = rOriginCamSpace.y - centerCamSpace.y;
+        double zDiff = rOriginCamSpace.z - centerCamSpace.z;
 
         double A = rx * rx + ry * ry + rz * rz;
-        double B = 2 * ( rx * (xDiff) + ry * (yDiff) + rz * ()  );
+        double B = 2 * (rx * (xDiff) + ry * (yDiff) + rz * (zDiff));
         double C = xDiff * xDiff + yDiff * yDiff + zDiff * zDiff - radius * radius;
 
         double D = B * B - 4 * A * C;
+
+        // System.out.println("B: " + B + "\nB * B : " + B * B + "\nC : " + C);
+        // System.out.println(D);
 
         if (D < 0) { // no roots
             return false;
