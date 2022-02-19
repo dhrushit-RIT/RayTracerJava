18add3a66a8c708e706513eae90c08307c5d5009
Dhrushit Raval <dr9703@rit.edu>
2022-02-19 17:38:21 -0500

    pixel class

--- src/Pixel.java
+++ src/Pixel.java
@@ -1,5 +1,37 @@
 public class Pixel {
-    private float value;
-    private float width;
-    private float height;
+    public int value;
+    public int row;
+    public int col;
+    public static double width;
+    public static double height;
+
+    // public Vector planePosition; // with respect to the origin of film plane
+    public Vector wPosition; // with respect to the origin of world
+
+    public int divisions = 1;
+
+    public Pixel(int row, int col, Vector position) {
+        // this.width = width;
+        // this.height = height;
+        this.row = row;
+        this.col = col;
+        this.wPosition = position;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setWidth(float width) {
+        Pixel.width = width;
+    }
+
+    public void setHeight(float height) {
+        Pixel.height = height;
+    }
+
+    public String toString() {
+        return "Pix pos: " + wPosition;
+    }
+
 }
