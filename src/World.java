4ce1d68d5400b80f7081b4110cf8d9ff69340c9a
Dhrushit Raval <dr9703@rit.edu>
2022-02-19 17:39:42 -0500

    world class

--- src/World.java
+++ src/World.java
@@ -1,4 +1,37 @@
-public class World {
+import java.util.ArrayList;
 
+public class World {
+    BoundingBox boundingBox;
+    private Camera camera;
+
+    ArrayList<Entity> worldObjects;
+
+    public World() {
+        this.worldObjects = new ArrayList<>();
+    }
+
+    public void addEntity(Entity entity) {
+        this.worldObjects.add(entity);
+    }
+
+    public void simulate() {
+
+        this.camera.takeASnap();
+        this.camera.generateImage();
+    }
+
+    public void setCamera(Camera camera) {
+        this.camera = camera;
+        this.camera.setWorld(this);
+    }
+
+    public boolean checkIntersection(Ray ray) {
+        for (Entity entity : worldObjects) {
+            if (entity.intersect(ray)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
