b1e9ffc6559a7f2d3fc2c38588ce1458f5aa40e4
Dhrushit Raval <dr9703@rit.edu>
2022-02-19 22:06:46 -0500

    adding two triangles to make up the plane

--- src/Application.java
+++ src/Application.java
@@ -9,6 +9,8 @@ public class Application {
         // add sphere to the world
         this.world.addEntity(this.getDefaultSphere1());
         this.world.addEntity(this.getDefaultSphere2());
+        this.world.addEntity(this.getDefaultTrainTriangle1());
+        this.world.addEntity(this.getDefaultTrainTriangle2());
         // TODO : add platform to the world
         // add camera to the world
         this.world.setCamera(camera);
@@ -16,35 +18,32 @@ public class Application {
         // get camera instance and snap
     }
 
+    private Triangle getDefaultTrainTriangle1() {
+        return new Triangle(null, new Point(1, 0, 1),
+                new Point[] { new Point(1, 0, 1), new Point(-1, 0, 1), new Point(1, 0, -1) });
+    }
+
+    private Triangle getDefaultTrainTriangle2() {
+        return new Triangle(null, new Point(1, 0, 1),
+                new Point[] { new Point(-1, 0, 1), new Point(1, 0, -1), new Point(-1, 0, -1) });
+    }
+
     private Sphere getDefaultSphere2() {
-        
-        // return new Sphere(new Point(1.5, 0, -0.5), 0.9, null);
         return new Sphere(new Point(0.6, 0.8, -0.6), 0.6, null);
     }
 
     private Sphere getDefaultSphere1() {
-        // 0.466789, -0.675721, 1.10661
-        // old : 2, -.585, 2.6
-        // radius = 0.755
-        // color = null
-        // return new Sphere(new Point(0, 0, 0), 1, null);
         return new Sphere(new Point(-0.4, 1, 0.4), 0.755, null);
-        // return new Sphere(new Point(0.466789, -0.675721, 1.10661), 1, null);
 
     }
 
     private Camera getDefaultCamera() {
-        // Point cameraPosition = new Point(0, 0, 5);
         Point cameraPosition = new Point(0, 1.15, 6);
-        // Point cameraPosition = new Point(5.5, -0.5, 1);
-        // an arbitary up works, it can later be projected to camera to work as its up : https://raytracing.github.io/books/RayTracingInOneWeekend.html#positionablecamera/cameraviewinggeometry
+        // an arbitary up works, it can later be projected to camera to work as its up :
+        // https://raytracing.github.io/books/RayTracingInOneWeekend.html#positionablecamera/cameraviewinggeometry
         // TODO : not sure why but figure it out why
-        Vector cameraUp = new Vector(0, 1, 0); // this is wrt camera. 
-        // Vector cameraUp = new Vector(0, 2.15, 6); 
-        // Vector cameraUp = new Vector(0, 0, 1);
-        // Vector cameraLookAt = new Vector(0, 0, -1); // this is wrt camera
-        Vector cameraLookAt = new Vector(0, 1.15, 5); 
-        // Vector cameraLookAt = new Vector(-1, 0, 0);
+        Vector cameraUp = new Vector(0, 1, 0); // this is wrt camera.
+        Vector cameraLookAt = new Vector(0, 1.15, 5);
         double cameraFocalLength = 2.5;
 
         return new Camera(cameraPosition, cameraUp, cameraLookAt, cameraFocalLength);
