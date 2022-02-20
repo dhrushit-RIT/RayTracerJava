f6014176d7fd1f7cd4080304ae863dd85c49be73
Dhrushit Raval <dr9703@rit.edu>
2022-02-19 22:06:07 -0500

    triangle class

---
+++ src/Triangle.java
@@ -0,0 +1,57 @@
+import org.ejml.simple.SimpleMatrix;
+
+public class Triangle extends Entity {
+
+    public Point wPosition;
+    public Point[] verticePoints;
+
+    private Vector e1;
+    private Vector e2;
+    private Vector T;
+    private Vector P;
+    private Vector Q;
+
+    public Triangle(Color baseColor, Point position, Point[] verticePoints) {
+        super(baseColor, position);
+        this.wPosition = position;
+        this.verticePoints = verticePoints;
+
+        this.e1 = Util.subtract(verticePoints[1], verticePoints[0]);
+        this.e2 = Util.subtract(verticePoints[2], verticePoints[0]);
+    }
+
+    @Override
+    public boolean intersect(Ray ray) {
+
+        if (this.T == null) {
+            this.T = Util.subtract(ray.origin, Camera.toCameraSpace(verticePoints[0]));
+            this.Q = Util.cross(T, e1);
+        }
+
+        Vector P = Util.cross(ray.direction, e2);
+
+        double Pe1 = Util.dot(P, e1);
+
+        if (Pe1 == 0) {
+            return false;
+        }
+        SimpleMatrix wuv = new SimpleMatrix(new double[][]{
+                {Util.dot(Q, e1)},
+                {Util.dot(P, T)},
+                {Util.dot(Q, ray.direction)}})
+                .divide(Pe1);
+
+        double w = wuv.get(0, 0);
+        double u = wuv.get(1, 0);
+        double v = wuv.get(2, 0);
+
+        if (w < 0) {
+            return false;
+        } else if (u < 0 || v < 0 || u + v > 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
