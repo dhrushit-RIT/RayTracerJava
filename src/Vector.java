c146f7d4b12fd2f962ece6e15382522b8c0d1489
Dhrushit Raval <dr9703@rit.edu>
2022-02-19 19:11:04 -0500

    fixing cross product rule

--- src/Vector.java
+++ src/Vector.java
@@ -154,7 +154,7 @@ public class Vector {
     public static Vector cross(Vector a, Vector b) {
         return new Vector(
                 a.y * b.z - a.z * b.y,
-                a.x * b.z - a.z * b.x,
+                a.z * b.x - a.x * b.z,
                 a.x * b.y - a.y * b.x);
     }
 
