d5e8f0ec713dba6ebfe9ec290d210f895a65dac9
Dhrushit Raval <dr9703@rit.edu>
2022-02-21 11:17:56 -0500

    setup the application from another file

--- src/Application.java
+++ src/Application.java
@@ -3,51 +3,16 @@ public class Application {
 
     private final World world;
 
+    public World getWorld() {
+        return world;
+    }
+
     public Application() {
         this.world = new World();
-        Camera camera = this.getDefaultCamera();
-        // add sphere to the world
-        this.world.addEntity(this.getDefaultSphere1());
-        this.world.addEntity(this.getDefaultSphere2());
-        this.world.addEntity(this.getDefaultTrainTriangle1());
-        this.world.addEntity(this.getDefaultTrainTriangle2());
-        // TODO : add platform to the world
-        // add camera to the world
-        this.world.setCamera(camera);
-
-        // get camera instance and snap
+        // Setups.setup0(this);
+        Setups.setup1(this);
     }
 
-    private Triangle getDefaultTrainTriangle1() {
-        return new Triangle(null, new Point(1, 0, 1),
-                new Point[] { new Point(1, 0, 1), new Point(-1, 0, 1), new Point(1, 0, -1) });
-    }
-
-    private Triangle getDefaultTrainTriangle2() {
-        return new Triangle(null, new Point(1, 0, 1),
-                new Point[] { new Point(-1, 0, 1), new Point(1, 0, -1), new Point(-1, 0, -1) });
-    }
-
-    private Sphere getDefaultSphere2() {
-        return new Sphere(new Point(0.6, 0.8, -0.6), 0.6, null);
-    }
-
-    private Sphere getDefaultSphere1() {
-        return new Sphere(new Point(-0.4, 1, 0.4), 0.755, null);
-
-    }
-
-    private Camera getDefaultCamera() {
-        Point cameraPosition = new Point(0, 1.15, 6);
-        // an arbitary up works, it can later be projected to camera to work as its up :
-        // https://raytracing.github.io/books/RayTracingInOneWeekend.html#positionablecamera/cameraviewinggeometry
-        // TODO : not sure why but figure it out why
-        Vector cameraUp = new Vector(0, 1, 0); // this is wrt camera.
-        Vector cameraLookAt = new Vector(0, 1.15, 5);
-        double cameraFocalLength = 2.5;
-
-        return new Camera(cameraPosition, cameraUp, cameraLookAt, cameraFocalLength);
-    }
 
     public void start() {
         this.world.simulate();
@@ -56,6 +21,5 @@ public class Application {
     public static void main(String[] args) {
         Application application = new Application();
         application.start();
-
     }
 }
