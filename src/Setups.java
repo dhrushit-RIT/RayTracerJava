42cc05c5a8df36f51e88fc979e9372b01757db1e
Dhrushit Raval <dr9703@rit.edu>
2022-02-22 15:14:19 -0500

    final setup

--- src/Setups.java
+++ src/Setups.java
@@ -11,7 +11,7 @@ public class Setups {
         double cameraFocalLength = 1.5;
         Camera camera = new Camera(cameraPosition, cameraUp, cameraLookAt, cameraFocalLength);
 
-        Point sphereCenter = new Point(0, 0, 2);
+        Point sphereCenter = new Point(0, 0, -3);
         double sphereRadius = 1.0;
         MyColor sphereColor = new MyColor(0, 0, 255);
         application.getWorld().addEntity(new Sphere(sphereCenter, sphereRadius, sphereColor));
@@ -23,9 +23,9 @@ public class Setups {
         // an arbitary up works, it can later be projected to camera to work as its up :
         // https://raytracing.github.io/books/RayTracingInOneWeekend.html#positionablecamera/cameraviewinggeometry
         // TODO : not sure why but figure it out why
-        Point cameraPosition = new Point(0, 1.15, 6);
+        Point cameraPosition = new Point(0, 1.15, 2.6);
         Vector cameraUp = new Vector(0, 1, 0); // this is wrt camera.
-        Vector cameraLookAt = new Vector(0, 1.15, 5);
+        Vector cameraLookAt = new Vector(0, 1.15, 2.5);
         double cameraFocalLength = 2.5;
         Camera camera = new Camera(cameraPosition, cameraUp, cameraLookAt, cameraFocalLength);
 
