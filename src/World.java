cc7ba51f911ef0371ceea0bfdc56a1398704a37d
Dhrushit Raval <dr9703@rit.edu>
2022-02-24 16:23:02 -0500

    updating check intersection details to return intersection details and not just entity

--- src/World.java
+++ src/World.java
@@ -5,13 +5,19 @@ public class World {
     private Camera camera;
 
     ArrayList<Entity> worldObjects;
+    ArrayList<Light> lightSources;
 
     public World() {
         this.worldObjects = new ArrayList<>();
+        this.lightSources = new ArrayList<>();
     }
 
     public void addEntity(Entity entity) {
         this.worldObjects.add(entity);
+    }
+
+    public void addLightSource(Light light) {
+        this.lightSources.add(light);
     }
 
     public void simulate() {
@@ -25,13 +31,22 @@ public class World {
         this.camera.setWorld(this);
     }
 
+    public MyColor getPixelIrradiance(Ray ray) {
+        IntersectionDetails entityIntersectionDetails = this.checkIntersection(ray);
+        for (Light light : lightSources) {
+            MyColor color  = entityIntersectionDetails.entity.getPixelIrradiance(light, this.camera, entityIntersectionDetails.intersectionPoint, entityIntersectionDetails.normalAtIntersection);
+            
+        }
+
+    }
+
     /**
      * check if any of the object is intersecting
      * 
      * @param ray
      * @return Entity that intersects the first with the ray
      */
-    public Entity checkIntersection(Ray ray) {
+    public IntersectionDetails checkIntersection(Ray ray) {
         Entity nearestEntity = null;
         double nearestDistance = 100000000;
         IntersectionDetails bestIntersection = new IntersectionDetails(100000000);
@@ -46,8 +61,10 @@ public class World {
             }
         }
 
+        bestIntersection.entity = nearestEntity;
+
         if (nearestDistance > 0) {
-            return nearestEntity;
+            return bestIntersection;
         } else {
             return null;
         }
