2d0505ad85699656b7072a6c0ca435eff66ecfa3
Dhrushit Raval <dr9703@rit.edu>
2022-02-24 16:23:18 -0500

    adding the entity to intersection details

--- src/IntersectionDetails.java
+++ src/IntersectionDetails.java
@@ -2,14 +2,16 @@ public class IntersectionDetails {
     public double distance;
     public Point intersectionPoint;
     public Vector normalAtIntersection;
+    public Entity entity;
 
     public IntersectionDetails(double distance){
         this.distance = distance;
     }
 
-    public IntersectionDetails() {
+    public IntersectionDetails(Entity entity) {
         this.distance = -1;
         this.intersectionPoint = null;
         this.normalAtIntersection = null;
+        this.entity = entity;
     }
 }
